8c6bc08e722bac6d2e30e4e4e854a9ac2c726e32
jokyxray@gmail.com <jokyxray@gmail.com>
2018-10-26 13:40:58 +0800

    update

---
+++ app/src/main/java/com/jokysss/mirror/widget/SlideMenu.java
@@ -0,0 +1,29 @@
+package com.jokysss.mirror.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class SlideMenu extends ViewGroup {
+
+    public SlideMenu(Context context) {
+        this(context, null);
+    }
+
+    public SlideMenu(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SlideMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init(Context context) {
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
