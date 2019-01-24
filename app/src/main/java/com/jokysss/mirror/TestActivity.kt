e997ba9e8ab8de20eacda44efdfd83a60dda5b8b
jokyxray@gmail.com <jokyxray@gmail.com>
2019-01-24 11:53:22 +0800

    update

--- app/src/main/java/com/jokysss/mirror/TestActivity.kt
+++ app/src/main/java/com/jokysss/mirror/TestActivity.kt
@@ -2,11 +2,27 @@ package com.jokysss.mirror
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.layout_test.*
 
 class TestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_test)
+        progress_circular.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                pathView.progress = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+            }
+        })
     }
 }
--- app/src/main/java/com/jokysss/mirror/widget/PathHoverView.kt
+++ app/src/main/java/com/jokysss/mirror/widget/PathHoverView.kt
@@ -5,14 +5,27 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import com.jokysss.mirror.R
+import kotlin.math.max
+import kotlin.math.min
 
 class PathHoverView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
     lateinit var firstBitmap: Bitmap
     lateinit var secondBitmap: Bitmap
-
+    var maxRadius = 0
+    var progress = 0
+        set(value) {
+            var tempVal = min(max(value, 0), 100)
+            field = tempVal
+            postInvalidate()
+        }
     init {
         firstBitmap = BitmapFactory.decodeResource(resources, R.mipmap.ff)
         secondBitmap = BitmapFactory.decodeResource(resources, R.mipmap.hh)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        maxRadius = Math.sqrt(w * w + h * h + 0.0).toInt()
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -24,7 +37,7 @@ class PathHoverView @JvmOverloads constructor(context: Context, attrs: Attribute
 
         var circlePath = Path()
 //        circlePath.setLastPoint(0F,0F)
-        circlePath.addCircle(0F, 0F, (width / 2).toFloat(), Path.Direction.CW)
+        circlePath.addCircle(0F, 0F, (maxRadius * progress / 100).toFloat(), Path.Direction.CW)
         leftPath.op(circlePath, Path.Op.INTERSECT)
         rightPath.op(circlePath, Path.Op.DIFFERENCE)
 
